70479381fe50b46373d31b40fe6c9705209e9c5c
news structure <maisonatelier52@gmail.com>
2026-01-16 16:00:44 +0530

    chnage about folder name

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,25 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "images.unsplash.com",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "randomuser.me",
+//       },
+//        {
+//         protocol: "https",
+//         hostname: "i.pravatar.cc", // Add this line for pravatar.cc
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -10,12 +32,22 @@ const nextConfig = {
         protocol: "https",
         hostname: "randomuser.me",
       },
-       {
+      {
         protocol: "https",
         hostname: "i.pravatar.cc", // Add this line for pravatar.cc
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/about",
+        destination: "/about-newswireninja",
+        permanent: true, // Permanent redirect (301)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
