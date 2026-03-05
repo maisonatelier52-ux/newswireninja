dd448e637541cd51ce189a8b8e2e7bfb059ef28e
news structure <maisonatelier52@gmail.com>
2026-03-05 10:16:19 +0530

    update on robots file

--- app/robots.js
+++ app/robots.js
@@ -1,23 +1,26 @@
+
+
 // export default function robots() {
 //   return {
-//     rules: [
-//       {
-//         userAgent: "*",
-//         allow: "/",
-//       },
-//     ],
-//     sitemap: "https://www.newswireninja.com/sitemap.xml",
-//     host: "https://www.newswireninja.com",
+//     rules: {
+//       userAgent: '*',
+//       allow: '/',
+//       disallow: ['/api/', '/_next/'],
+//     },
+//     sitemap: 'https://www.newswireninja.com/sitemap.xml',
 //   };
 // }
 
 export default function robots() {
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/_next/'],
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/api/", "/_next/"],
     },
-    sitemap: 'https://www.newswireninja.com/sitemap.xml',
+    sitemap: [
+      "https://www.newswireninja.com/sitemap.xml",
+      "https://www.newswireninja.com/news-sitemap.xml"
+    ],
   };
 }
