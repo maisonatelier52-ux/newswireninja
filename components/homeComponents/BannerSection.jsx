27ad4f577170e7be13de3612b5b544d3e7f66121
news structure <maisonatelier52@gmail.com>
2026-06-01 14:36:00 +0530

    add a 13 th politics news

--- components/homeComponents/BannerSection.jsx
+++ components/homeComponents/BannerSection.jsx
@@ -4,7 +4,7 @@ export function BannerSection() {
   return (
     <section className="mt-12">
       <div className="relative w-full overflow-hidden rounded-md h-[70px] sm:h-[100px] md:h-[100px] lg:h-[140px] xl:h-[170px]">
-        <Image src="/images/thatscapitalism.webp" alt="That's Capitalism" fill priority className="object-fit" />
+        <Image src="/images/newswireninja_ads_banner.webp" alt="That's newswireninja" fill priority className="object-fit" />
       </div>
     </section>
   );
